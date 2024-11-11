535301ca82ca2311ce5f034f36049e76e8910df9
shanebeee <shanebolenback@me.com>
2024-11-11 12:28:49 -0800

    SkriptRegistrations - remove biome registration as Skript now dynamically does this

--- src/main/java/com/shanebeestudios/arc/api/data/SkriptRegistrations.java
+++ src/main/java/com/shanebeestudios/arc/api/data/SkriptRegistrations.java
@@ -4,25 +4,17 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.Aliases;
 import ch.njol.skript.aliases.AliasesProvider;
 import ch.njol.skript.aliases.InvalidMinecraftIdException;
-import ch.njol.skript.util.Version;
-import com.shanebeestudios.arc.api.util.SkriptUtil;
 import com.shanebeestudios.arc.api.util.Util;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.block.Biome;
 
 import java.util.HashMap;
-import java.util.Locale;
 
 public class SkriptRegistrations {
 
     public static void registerStuff() {
         registerCustomAliases();
         ModEntityType.registerCustomEntityTypes();
-        if (!Skript.getVersion().isLargerThan(new Version(2,9))) {
-            // In Skript 2.9+ biomes are dynamically registered via Registry
-            registerBiomes();
-        }
     }
 
     /**
@@ -48,23 +40,6 @@ public class SkriptRegistrations {
             }
         }
         Util.log("Registered &b%s&7 Modded Item Aliases", (addonProvider.getAliasCount() - aliasStartCount));
-    }
-
-    private static void registerBiomes() {
-        SkriptUtil.addToLangFile(langMap -> {
-            int biomes = 0;
-            for (Biome biome : Biome.values()) {
-                if (biome == Biome.CUSTOM) continue;
-
-                NamespacedKey key = biome.getKey();
-                if (key.getNamespace().contains("minecraft")) continue;
-
-                String acrKey = key.toString().replace(":", "_").toLowerCase(Locale.ROOT);
-                langMap.put("biomes." + acrKey, key.toString());
-                biomes++;
-            }
-            Util.log("Registered &b%s &7Modded Biomes", biomes);
-        });
     }
 
 }
